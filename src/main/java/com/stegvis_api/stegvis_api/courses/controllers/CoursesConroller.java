4c11f95aa6e5e647029d7ff04563090927076fc1
fishenchips <philipcarlsson@hotmail.com>
2025-10-30 22:01:27 +0100

    courses service and controller logic

--- src/main/java/com/stegvis_api/stegvis_api/courses/controllers/CoursesConroller.java
+++ src/main/java/com/stegvis_api/stegvis_api/courses/controllers/CoursesConroller.java
@@ -3,22 +3,25 @@ package com.stegvis_api.stegvis_api.courses.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import com.stegvis_api.stegvis_api.courses.entities.CourseDTO;
+import com.stegvis_api.stegvis_api.courses.model.enums.Courses;
+import com.stegvis_api.stegvis_api.courses.services.CoursesService;
 
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/courses")
 public class CoursesConroller {
 
-    @PostMapping("path")
-    public String postMethodName(@RequestBody String entity) {
-        //TODO: process POST request
-        
-        return entity;
+    private CoursesService service;
+
+    @GetMapping("/{course}")
+    public CourseDTO getCourse(@PathVariable Courses course) {
+        return service.getCourse(course);
     }
-    
-    
+
 }
